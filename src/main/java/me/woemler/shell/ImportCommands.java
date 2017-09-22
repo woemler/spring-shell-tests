25dc543db05176eaef97126790b9d23baca4c243
woemler <willoemler@gmail.com>
2017-09-21 21:57:21 -0400

    Some additional arg customization.

--- src/main/java/me/woemler/shell/ImportCommands.java
+++ src/main/java/me/woemler/shell/ImportCommands.java
@@ -1,12 +1,9 @@
 package me.woemler.shell;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import java.io.File;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
-import org.springframework.stereotype.Component;
 
 /**
  * @author woemler
@@ -14,16 +11,16 @@ import org.springframework.stereotype.Component;
 @ShellComponent
 public class ImportCommands {
 
-  private static final Logger logger = LoggerFactory.getLogger(ImportCommands.class);
+  public static enum DataType { SAMPLE, GENE, DATA }
 
-  @ShellMethod("import")
+  @ShellMethod(key = "import", value = "Imports the a file of a specified type.")
   public String fileImport(
-      @ShellOption String file,
-      @ShellOption String dataType,
-      @ShellOption(defaultValue = "false") Boolean overwrite
+      @ShellOption({"-f", "--file"}) File file,
+      @ShellOption({"-t", "--type"}) DataType dataType,
+      @ShellOption(value = {"-o", "--overwrite"}, defaultValue = "false") Boolean overwrite
   ){
-    logger.info(String.format("Importing file=%s  dataType=%s  overwrite=%s", file, dataType, overwrite));
-    return file;
+    System.out.println(String.format("Importing file=%s  dataType=%s  overwrite=%s", file, dataType, overwrite));
+    return file.getAbsolutePath();
   }
 
 
